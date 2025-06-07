258972ca268afcb7e52cdc882dbf297057eb1dcc
Dave Patton <dave.patton@gmail.com>
2025-06-07 22:24:16 +0000

    Resetting

--- tests/test_level_loader.py
+++
@@ -1,8 +0,0 @@
-from emerson_game.core.level_loader import LevelLoader
-
-
-def test_level_loader() -> None:
-    level = LevelLoader.load("emerson_game/levels/level_1.json")
-    assert level.width == 10
-    assert level.player is not None
-    assert len(level.entities) >= 1
